aba6724d3a076441714da989b8a60568f4775df6
Gabryel Benedito Monteiro <162363126+Gabryel-benedito@users.noreply.github.com>
2025-08-19 22:33:33 -0300

    Add files via upload

---
+++ aula/processa_post.php
@@ -0,0 +1,17 @@
+
+
+<?php 
+
+
+
+
+    $Nome = $_POST['nome'];
+    $email = $_POST['email'];  
+    $idade = $_POST['idade'];
+
+    echo " seus dados: <br>
+    Nome: $Nome <br>
+    E-mail: $email <br>
+    Idade: $idade <br>";    
+
+?>
